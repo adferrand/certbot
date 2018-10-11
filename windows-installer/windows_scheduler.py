cac869ba2fae73d4649099dd15871281cf2eb1cf
Adrien Ferrand <adrien.ferrand@amadeus.com>
2018-10-11 16:06:12 +0200

    Functional certbot renew task in Windows tasks scheduler.

--- windows-installer/windows_scheduler.py
+++ windows-installer/windows_scheduler.py
@@ -12,14 +12,16 @@ TASK_STATES_MAP = {
     4: 'Running'
 }
 
-class TaskNotFoundError(Exception): pass
-class AdministrativeRightsRequiredError(Exception): pass
+class TaskNotFoundError(Exception): 
+    pass
+class AdministrativeRightsRequiredError(Exception): 
+    pass
 
 def powershell_command(command):
-    p = subprocess.Popen(['C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe', '-Command', command],
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
-    (stdout, stderr) = p.communicate()
+    process = subprocess.Popen(['C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe', '-Command', command],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    (stdout, stderr) = process.communicate()
 
     return (stdout.decode('cp1252'), stderr.decode('cp1252'))
 
@@ -28,10 +30,10 @@ def raise_for_non_administrative_windows_rights():
         raise AdministrativeRightsRequiredError
 
 def get_scheduled_task():
-    (stdout, stderr) = powershell_command('Get-ScheduledTask -TaskName "CertbotRenew" -ErrorAction Ignore | ConvertTo-Json')
+    (stdout, stderr) = powershell_command('Get-ScheduledTask -TaskName "Certbot Renew Task" -ErrorAction Ignore | ConvertTo-Json')
 
     if stderr:
-        raise ValueError('''Error encountered while getting the CertbotRenew task: 
+        raise ValueError('''Error encountered while getting the Certbot Renew Task task: 
 {0}'''.format(stderr))
 
     if not stdout:
@@ -47,62 +49,62 @@ def show_task_status():
 def create_task():
     raise_for_non_administrative_windows_rights()
     try:
-        status = get_scheduled_task()
-        print('The task CertbotRenew already exists.')
+        get_scheduled_task()
+        print('The task Certbot Renew Task already exists.')
     except TaskNotFoundError:
         (_, stderr) = powershell_command('''
-$action = New-ScheduledTaskAction -Execute 'certbot' -Argument 'renew'
-$trigger1 = New-ScheduledTaskTrigger -Daily -At 12pm
-$trigger2 = New-ScheduledTaskTrigger -Daily -At 12am
-Register-ScheduledTask -Action $action -Trigger $trigger1,$trigger2 -TaskName "CertbotRenew" -Description "Excute twice a day the certbot renew command"
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -Command "certbot renew"'
+$triggerAM = New-ScheduledTaskTrigger -Daily -At 12am
+$triggerPM = New-ScheduledTaskTrigger -Daily -At 12pm
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType S4U -RunLevel Highest
+Register-ScheduledTask -Action $action -Trigger $triggerAM,$triggerPM -TaskName "Certbot Renew Task" -Description "Execute twice a day the 'certbot renew' command, to renew Let's Encrypt certificates if needed." -Principal $principal
 ''')
 
         if stderr:
-            raise ValueError('''Error encountered while creating the CertbotRenew task: 
+            raise ValueError('''Error encountered while creating the Certbot Renew Task task: 
 {0}'''.format(stderr))
 
-    print('The task CertbotRenew has been created and is enabled.')
+    print('The task Certbot Renew Task has been created and is enabled.')
 
 def delete_task():
     raise_for_non_administrative_windows_rights()
     get_scheduled_task()
 
-    (_, stderr) = powershell_command('Unregister-ScheduledTask -TaskName "CertbotRenew" -Confirm:$false')
+    (_, stderr) = powershell_command('Unregister-ScheduledTask -TaskName "Certbot Renew Task" -Confirm:$false')
 
     if stderr:
-        raise ValueError('''Error encountered while deleting the CertbotRenew task: 
+        raise ValueError('''Error encountered while deleting the Certbot Renew Task task: 
 {0}'''.format(stderr))
 
-    print('The task CertbotRenew has been deleted.')
+    print('The task Certbot Renew Task has been deleted.')
 
 def enable_task():
     raise_for_non_administrative_windows_rights()
     get_scheduled_task()
 
-    (_, stderr) = powershell_command('Enable-ScheduledTask -TaskName "CertbotRenew"')
+    (_, stderr) = powershell_command('Enable-ScheduledTask -TaskName "Certbot Renew Task"')
 
     if stderr:
-        raise ValueError('''Error encountered while enabling the CertbotRenew task: 
+        raise ValueError('''Error encountered while enabling the Certbot Renew Task task: 
 {0}'''.format(stderr))
 
-    print('The task CertbotRenew has been enabled.')
+    print('The task Certbot Renew Task has been enabled.')
 
 def disable_task():
     raise_for_non_administrative_windows_rights()
     get_scheduled_task()
 
-    (_, stderr) = powershell_command('Disable-ScheduledTask -TaskName "CertbotRenew"')
+    (_, stderr) = powershell_command('Disable-ScheduledTask -TaskName "Certbot Renew Task"')
 
     if stderr:
-        raise ValueError('''Error encountered while disabling the CertbotRenew task: 
+        raise ValueError('''Error encountered while disabling the Certbot Renew Task task: 
 {0}'''.format(stderr))
 
-    print('The task CertbotRenew has been disabled.')
+    print('The task Certbot Renew Task has been disabled.')
 
 def build_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument
-    subparsers = parser.add_subparsers(help='Sub-commands available on the CertbotRenew task.')
+    subparsers = parser.add_subparsers(help='Sub-commands available on the Certbot Renew Task task.')
 
     status = subparsers.add_parser('status', help='Show the task status.')
     status.set_defaults(function=show_task_status)
@@ -122,7 +124,7 @@ def build_parser():
 
     return parser
 
-if __name__ == '__main__':
+def main():
     parser = build_parser()
     args = parser.parse_args()
 
@@ -130,8 +132,11 @@ if __name__ == '__main__':
         try:
             args.function()
         except TaskNotFoundError:
-            print('Task CertbotRenew does not exist.\nPlease create it using the following command: \'certbot-scheduler create\'.')
+            print('Task Certbot Renew Task does not exist.\nPlease create it using the following command: \'certbot-scheduler create\'.')
             sys.exit(1)
         except AdministrativeRightsRequiredError:
             print('Error, subcommand must be run on a shell with administrative rights.')
             sys.exit(1)
+
+if __name__ == '__main__':
+    main()
