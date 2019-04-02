51402ea2bb6072219f7fb609b344cdc02768e944
Adrien Ferrand <ferrand.ad@gmail.com>
2019-04-02 17:44:57 +0200

    Build a custom checker to forbid os module usage

--- linter_plugin.py
+++ linter_plugin.py
@@ -1,14 +1,47 @@
-"""Certbot ACME PyLint plugin.
+"""Certbot PyLint plugin.
 
 http://docs.pylint.org/plugins.html
 
 """
 from astroid import MANAGER
 from astroid import nodes
+from pylint.checkers import BaseChecker
+from pylint.interfaces import IAstroidChecker
+from pylint.checkers.utils import check_messages
 
 
-def register(unused_linter):
+def register(linter):
     """Register this module as PyLint plugin."""
+    linter.register_checker(ForbidStandardOsModule(linter))
+
+
+class ForbidStandardOsModule(BaseChecker):
+    """
+    This checker ensures that standard os module is not imported by certbot classes.
+    Otherwise a 'os-module-forbidden' error will be registered for the faulty lines.
+    """
+    __implements__ = IAstroidChecker
+
+    name = 'forbid-os-module'
+    msgs = {
+        'E5001': (
+            'Use of os module is forbidden in Certbot. Use certbot.compat.os instead.',
+            'os-module-forbidden',
+            'Some methods from standard os modules cannot be used for security reasons on Windows.'
+        )
+    }
+    priority = -1
+
+    @check_messages('os-module-forbidden')
+    def visit_import(self, node):
+        if 'os' in [alias.name for alias in node.names]:
+            self.add_message('os-module-forbidden', node=node)
+
+    @check_messages('os-module-forbidden')
+    def visit_importfrom(self, node):
+        if node.module == 'node':
+            self.add_message('os-module-forbidden', node=node)
+
 
 def _transform(cls):
     # fix the "no-member" error on instances of
