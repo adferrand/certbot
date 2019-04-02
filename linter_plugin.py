c7524daf4d2765e13ab3361b5b0c9c5334f896e5
Adrien Ferrand <ferrand.ad@gmail.com>
2019-04-02 23:09:06 +0200

    Test without fix

--- linter_plugin.py
+++ linter_plugin.py
@@ -3,8 +3,6 @@
 http://docs.pylint.org/plugins.html
 
 """
-from astroid import MANAGER
-from astroid import nodes
 from pylint.checkers import BaseChecker
 from pylint.interfaces import IAstroidChecker
 from pylint.checkers.utils import check_messages
@@ -44,22 +42,3 @@ class ForbidStandardOsModule(BaseChecker):
     def visit_importfrom(self, node):
         if node.modname == 'os':
             self.add_message('os-module-forbidden', node=node)
-
-
-def _transform(cls):
-    # fix the "no-member" error on instances of
-    # letsencrypt.acme.util.ImmutableMap subclasses (instance
-    # attributes are initialized dynamically based on __slots__)
-
-    # TODO: this is too broad and applies to any tested class...
-
-    if cls.slots() is not None:
-        for slot in cls.slots():
-            cls.locals[slot.value] = [nodes.EmptyNode()]
-
-    if cls.name == 'JSONObjectWithFields':
-        # _fields is magically introduced by JSONObjectWithFieldsMeta
-        cls.locals['_fields'] = [nodes.EmptyNode()]
-
-
-MANAGER.register_transform(nodes.Class, _transform)
