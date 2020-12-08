9519943ea8e67f1e0f9990a290a859ed2afee48f
Adrien Ferrand <ferrand.ad@gmail.com>
2020-12-08 14:05:02 +0100

    Remove a call

--- certbot-ci/certbot_integration_tests/assets/farm_scripts/test_certbot_apache2.sh
+++ certbot-ci/certbot_integration_tests/assets/farm_scripts/test_certbot_apache2.sh
@@ -23,7 +23,6 @@ elif yum --version >/dev/null 2>&1; then
   curl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl.py -o /usr/bin/systemctl
   chmod +x /usr/bin/systemctl
   yum -y install httpd
-  yum -y install nghttp2 || true
   systemctl start httpd
   mkdir -p "/var/www/${PUBLIC_HOSTNAME}/public_html"
   chmod -R oug+rwx /var/www
